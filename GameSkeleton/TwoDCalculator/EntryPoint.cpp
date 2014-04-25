4b872d8cf6570c2af53d43e0a550c184060ce754
brollins <brollins@e569a64c-822d-f44b-be61-ecb32803aff7>
2014-04-25 21:27:14 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/csc190blake_rollins@6 e569a64c-822d-f44b-be61-ecb32803aff7

--- GameSkeleton/TwoDCalculator/EntryPoint.cpp
+++ GameSkeleton/TwoDCalculator/EntryPoint.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 //using Engine::Vector2;
 
-Engine::Vector2D basicLeft(0,0), basicRight(0,0), basicResult(0,0);
+Engine::Vector2D basicLeft, basicRight, basicResult;
 
 void myBasicVectorEquationCallback(const BasicVectorEquationInfo& data)
 {
@@ -31,16 +31,93 @@ void myBasicVectorEquationCallback(const BasicVectorEquationInfo& data)
 	//cout << "resy: " << basicResult.y << endl;
 }
 
+Engine::Vector2D perpOrig, perpNormal, perpCW, perpCCW;
+
+void myPerpendicularDataCallback(const PerpendicularData& data)
+{
+	// TODO
+	perpOrig.x = data.x;
+	perpOrig.y = data.y;
+
+}
+
+Engine::Vector2D leN;
+float leP, leD;
+
+void myLineEquationDataCallback(const LineEquationData& data)
+{
+	// TODO
+	leD = data.d;
+	leN.x = data.n_i;
+	leN.y = data.n_j;
+	leP = data.p_x;
+}
+
+Engine::Vector2D dot1, dot2, dotProj, dotRej;
+
+void myDotProductDataCallback(const DotProductData& data)
+{
+	// TODO
+	data.projectOntoLeftVector;
+	dot1.x = data.v1i;
+	dot1.y = data.v1j;
+	dot2.x = data.v2i;
+	dot2.y = data.v2j;
+}
+
+Engine::Vector2D lerpA, lerpB, lerpAPart, lerpBPart, lerpRes, lerpMinB;
+
+void myLerpDataCallback(const LerpData& data)
+{
+	// TODO
+	data.a_i;
+	data.a_j;
+	data.beta;
+	data.b_i;
+	data.b_j;
+}
+
+
 int main(int argc, char* argv[])
 {
 	Engine::Init();
 
 	RenderUI renderUI;
 
+	// Vector Basics
 	renderUI.setBasicVectorEquationData(myBasicVectorEquationCallback,
 		(float*)&basicLeft,
 		(float*)&basicRight,
 		(float*)&basicResult);
+
+	// Perpendiculars
+	renderUI.setPerpendicularData((float*)&perpOrig,
+		(float*)&perpNormal,
+		(float*)&perpCW,
+		(float*)&perpCCW,
+		myPerpendicularDataCallback);
+
+	// Line Equation
+	renderUI.setLineEquationData((float*)&leP,
+		(float*)&leN,
+		(float*)&leD,
+		myLineEquationDataCallback);
+
+	// Dot Product
+	renderUI.setDotProductData((float*)&dot1,
+		(float*)&dot2,
+		(float*)&dotProj,
+		(float*)&dotRej,
+		myDotProductDataCallback);
+
+	// LERP
+	renderUI.setLerpData((float*)&lerpA,
+		(float*)&lerpB,
+		(float*)&lerpMinB,
+		(float*)&lerpAPart,
+		(float*)&lerpBPart,
+		(float*)&lerpRes,
+		myLerpDataCallback);
 
 	if( ! renderUI.initialize(argc, argv))
 		return -1;
